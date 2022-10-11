542be95a5c7cdda4a23e1f97dfcf00e5e55d946b
egor0665 <galancc@mail.ru>
2022-10-11 20:24:05 +0300

    just works

--- app/src/main/java/com/example/kyrsach/DeviceRepository.kt
+++ app/src/main/java/com/example/kyrsach/DeviceRepository.kt
@@ -8,17 +8,20 @@ import android.bluetooth.BluetoothSocket
 import android.content.Context
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.delay
 import java.io.IOException
 import java.io.InputStream
 import java.io.OutputStream
 import java.util.*
+import java.util.logging.Handler
+import kotlin.concurrent.thread
 
 @SuppressLint("MissingPermission")
 class DeviceRepository(applicationContext: Context) {
     private val tag = "BluetoothConnection"
 
     private var counter = 0
-    private val tempData = TempData(null, null, null)
+    private val tempData = TempData(null, null, null, null, null, null, null, null)
 
     private var btAdapter: BluetoothAdapter? = null
     private var btSocket: BluetoothSocket? = null
@@ -68,7 +71,7 @@ class DeviceRepository(applicationContext: Context) {
             inStream = btSocket!!.inputStream
             receiveThread = ReceiveThread()
             receiveThread?.start()
-            sendData(0)
+            sendData(0xff)
             Log.d(tag, "Поток создан")
             return true
 
@@ -92,6 +95,8 @@ class DeviceRepository(applicationContext: Context) {
         msgBuffer[0] = data.toByte()
 
         return try {
+
+            Thread.sleep(200)
             outStream!!.write(msgBuffer)
             Log.d("Success", "Оправлены: $data")
             true
@@ -104,22 +109,29 @@ class DeviceRepository(applicationContext: Context) {
 
     private fun receiveData() {
         val msgBuffer = ByteArray(1)
+        Log.d("Success", msgBuffer.toString())
         while (true) {
             try {
                 val size = inStream?.read(msgBuffer)
                 counter = counter.inc()
                 when (counter) {
-                    1 -> tempData.id = msgBuffer[0].toInt()
-                    2 -> tempData.date = msgBuffer[0].toInt()
-                    3 -> {
-                        tempData.info = msgBuffer[0].toInt()
+                    1 -> tempData.sec = msgBuffer[0].toUByte()
+                    2 -> tempData.min = msgBuffer[0].toUByte()
+                    3 -> tempData.hr = msgBuffer[0].toUByte()
+                    4 -> tempData.day = msgBuffer[0].toUByte()
+                    5 -> tempData.mnth = msgBuffer[0].toUByte()
+                    6 -> tempData.yearfh = msgBuffer[0].toUByte()
+                    7 -> tempData.yearsh = msgBuffer[0].toUByte()
+                    8 -> {
+                        tempData.dayofweek = msgBuffer[0].toUByte()
                         counter = 0
                         info.postValue(tempData)
                     }
                 // 3 байта
                 }
-                Log.d("Success", "Message: ${msgBuffer[0].toInt()}")
+                Log.d("Success", "Message: ${msgBuffer[0].toUByte()}")
             } catch (i: Exception) {
+                Log.d("Success", "Successless")
                 break
             }
         }
--- app/src/main/java/com/example/kyrsach/MainActivity.kt
+++ app/src/main/java/com/example/kyrsach/MainActivity.kt
@@ -1,56 +1,108 @@
 package com.example.kyrsach
 
 import android.Manifest
-import android.bluetooth.BluetoothDevice
-import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.icu.text.AlphabeticIndex
 import android.net.Uri
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
-import android.view.View
 import android.widget.Button
+import android.widget.DatePicker
 import android.widget.TextView
+import android.widget.TimePicker
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var connectButton: View
-    private lateinit var getButton: View
-    private lateinit var setButton: View
+    private lateinit var connectButton: Button
+    private lateinit var getButton: Button
+    private lateinit var setButton: Button
+    private lateinit var datePicker: DatePicker
+    private lateinit var timePicker: TimePicker
+    private lateinit var dateTime: TextView
     private lateinit var deviceRepository: DeviceRepository
     private var permissionGranted = false
     private var devices: List<Device>? = emptyList()
     private var address = ""
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        connectButton = findViewById<Button>(R.id.connectButton)
-        getButton = findViewById<Button>(R.id.getTimeButton)
-        setButton = findViewById<Button>(R.id.setTimeButton)
+        connectButton = findViewById(R.id.connectButton)
+        getButton = findViewById(R.id.getTimeButton)
+        setButton = findViewById(R.id.setTimeButton)
+        datePicker = findViewById(R.id.datePicker)
+        timePicker = findViewById(R.id.timePicker)
+        dateTime = findViewById(R.id.dateTime)
         deviceRepository =  DeviceRepository(this)
         getButton.isEnabled = false
         setButton.isEnabled = false
         getButton.setOnClickListener {
-
+            deviceRepository.sendData(0xff)
         }
         setButton.setOnClickListener {
-            deviceRepository.sendData(1224) //16 число
+            deviceRepository.sendData(0)
+            deviceRepository.sendData(timePicker.minute.div(10)*16+timePicker.minute %10)
+            deviceRepository.sendData(timePicker.hour.div(10)*16+timePicker.hour %10)
+            deviceRepository.sendData(datePicker.dayOfMonth.div(10)*16+datePicker.dayOfMonth %10)
+            deviceRepository.sendData((datePicker.month+1).div(10)*16+(datePicker.month+1) %10)
+            deviceRepository.sendData((datePicker.year % 100).div(10)*16+(datePicker.year % 100) %10)
+            deviceRepository.sendData(datePicker.year.div(100).div(10)*16+datePicker.year.div(100) %10)
+
+
+            val cal = Calendar.getInstance()
+            cal[Calendar.DAY_OF_MONTH] = datePicker.dayOfMonth
+            cal[Calendar.MONTH] = datePicker.month
+            cal[Calendar.YEAR] = datePicker.year
+            val simpledateformat = SimpleDateFormat("EEEE")
+            val date = Date(cal.getTimeInMillis())
+            var dayOfWeek = 0
+            Log.d("day" ,simpledateformat.format(date))
+            when( simpledateformat.format(date)){
+                "понедельник" -> dayOfWeek = 1
+                "вторник" -> dayOfWeek = 2
+                "среда" -> dayOfWeek = 3
+                "четверг" -> dayOfWeek = 4
+                "пятница" -> dayOfWeek = 5
+                "суббота" -> dayOfWeek = 6
+                "воскресенье" -> dayOfWeek = 7
+            }
+            deviceRepository.sendData(dayOfWeek)
+
         }
         connectButton.setOnClickListener{
-            connect(address)
+            GlobalScope.launch { deviceRepository.connect(address) }
         }
         findDevices()
         deviceRepository.startObserve().observe(this){
+            var sec = (it.sec!!.toInt().div(16)*10+it.sec!!.toInt() % 16).toString()
+            if (sec.length<2) sec = "0"+sec
+            var min = (it.min!!.toInt().div(16)*10+it.min!!.toInt() % 16).toString()
+            if (min.length<2) min = "0"+min
+            var hr = (it.hr!!.toInt().div(16)*10+it.hr!!.toInt() % 16).toString()
+            if (hr.length<2) hr = "0"+hr
+            var day = (it.day!!.toInt().div(16)*10+it.day!!.toInt() % 16).toString()
+            if (day.length<2) day = "0"+day
+            var mnth = (it.mnth!!.toInt().div(16)*10+it.mnth!!.toInt() % 16).toString()
+            if (mnth.length<2) mnth = "0"+mnth
+            var yearfh = (it.yearfh!!.toInt().div(16)*10+it.yearfh!!.toInt() % 16).toString()
 
+            var yearsh = (it.yearsh!!.toInt().div(16)*10+it.yearsh!!.toInt() % 16).toString()
+            if (yearsh.length<2) yearsh = "0"+yearsh
+            var dayofweek = (it.dayofweek!!.toInt().div(16)*10+it.dayofweek!!.toInt() % 16).toString()
+
+            dateTime.text = day+"."+ mnth+ "."+yearfh+ yearsh+ " "+hr+ ":"+min+ ":"+sec+" "+dayofweek
         }
         getLocationPermission()
     }
@@ -145,9 +197,7 @@ class MainActivity : AppCompatActivity() {
 
 }
 
-data class Data(val id: Int, val info: String?, val date: String?)
-
-data class TempData(var id: Int?, var date: Int?, var info: Int?)
+data class TempData(var sec: UByte?, var min: UByte?, var hr: UByte?, var day: UByte?, var mnth: UByte?, var yearfh: UByte?, var yearsh: UByte?, var dayofweek: UByte?)
 
 data class Device (val name: String, val address: String)
 
